ba19371b2176eddd98d9f3d792a7e0ceb2391cef
M Wahyu Ramdhani <95848079+wahramdhani@users.noreply.github.com>
2022-06-14 19:06:21 +0700

    FE:login-page

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "../../components/LoginForm";
+
+const Login = () => {
+  return (
+    <main className="mx-auto relative">
+      <LoginForm />
+    </main>
+  );
+};
+
+export default Login;
