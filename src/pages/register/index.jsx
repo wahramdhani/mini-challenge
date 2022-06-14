4fe2f4af84a2b6ea35ff6bb824c1a4c082aa2619
M Wahyu Ramdhani <95848079+wahramdhani@users.noreply.github.com>
2022-06-14 18:42:53 +0700

    FE:register-page

---
+++ src/pages/register/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterForm from "../../components/RegisterForm";
+
+const Register = () => {
+  return (
+    <main className="mx-auto relative">
+      <RegisterForm />
+    </main>
+  );
+};
+
+export default Register;
