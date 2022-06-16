6bcfffc10209b0d870710defbeb7b0264d722796
M Wahyu Ramdhani <95848079+wahramdhani@users.noreply.github.com>
2022-06-16 19:50:29 +0700

    FE:productList

---
+++ src/components/ProductLists/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
+
+  return {
+    props: { collections: data },
+  };
+};
+
+const ProductLists = ({ collections }) => {
+  return (
+    <div className="bg-white">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">Products</h2>
+
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {collections.map((collection) => (
+            <a key={collection.id} className="group">
+              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                <img
+                  src={collection.image}
+                  className="w-full h-full object-center object-cover group-hover:opacity-75"
+                />
+              </div>
+              <h3 className="mt-4 text-sm text-gray-700">{collection.title}</h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">
+                {collection.price}
+              </p>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductLists;
---
+++ src/pages/productList/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductLists from "../../components/ProductLists";
+
+const ProductList = () => {
+  return (
+    <main className="mx-auto relative">
+      <ProductLists />
+    </main>
+  );
+};
+
+export default ProductList;
