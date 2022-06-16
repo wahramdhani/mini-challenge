a4383f4f39c6c98853aca53573672c11164bfc95
M Wahyu Ramdhani <95848079+wahramdhani@users.noreply.github.com>
2022-06-16 20:31:38 +0700

    FE:pL still error

--- src/components/ProductLists/index.jsx
+++ src/components/ProductLists/index.jsx
@@ -16,20 +16,24 @@ const ProductLists = ({ collections }) => {
         <h2 className="sr-only">Products</h2>
 
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {collections.map((collection) => (
-            <a key={collection.id} className="group">
-              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src={collection.image}
-                  className="w-full h-full object-center object-cover group-hover:opacity-75"
-                />
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">{collection.title}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">
-                {collection.price}
-              </p>
-            </a>
-          ))}
+          {collections.map(collection => {
+            return (
+              <a key={collection.id}>
+                <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                  <img
+                    src={collection.image}
+                    className="w-full h-full object-center object-cover group-hover:opacity-75"
+                  />
+                </div>
+                <h3 className="mt-4 text-sm text-gray-700">
+                  {collection.title}
+                </h3>
+                <p className="mt-1 text-lg font-medium text-gray-900">
+                  {collection.price}
+                </p>
+              </a>
+            );
+          })}
         </div>
       </div>
     </div>
